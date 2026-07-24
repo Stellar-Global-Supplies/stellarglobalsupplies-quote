53016be44d274c56f360b2ae2e19ec695c47409f
Prasad Bhavsar <prasadbhavsar@Prasads-MacBook-Air.local>
2026-07-24 22:28:28 +0530

    fix: use span_exporter instead of exporter in BatchSpanProcessor

--- backend/lambda/tracing.py
+++ backend/lambda/tracing.py
@@ -102,7 +102,7 @@ def _create_tracer_provider() -> TracerProvider:
     )
 
     processor = BatchSpanProcessor(
-        exporter=exporter,
+        span_exporter=exporter,
         max_queue_size=2048,
         max_export_batch_size=512,
         schedule_delay_millis=5000,
